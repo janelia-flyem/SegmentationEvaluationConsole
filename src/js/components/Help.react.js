416a7db83f1e83b3cfafac4d34e1057fa246f556
Stephen Plaza <plazas@janelia.hhmi.org>
2015-07-26 22:03:14 -0400

    adds forgotten help file

---
+++ src/js/components/Help.react.js
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var Help = React.createClass({
+    render: function () {
+        return (
+            <div className="container">
+
+            <p>General documentation and installation information can be found on the github <a href="https://github.com/janelia-flyem/SegmentationEvaluationConsole">README</a>.  A user guide is available on the corresponding <a href="https://github.com/janelia-flyem/SegmentationEvaluationConsole/wiki">wiki</a>.</p>
+            </div>
+        );
+    }
+});
+
+
+module.exports = Help;
